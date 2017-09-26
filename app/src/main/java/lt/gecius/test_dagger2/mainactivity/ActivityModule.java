8b0e2720e5aeff029b51c93052246400b5bdb01d
Egis <e.gecius@gmail.com>
2017-09-26 08:15:06 +0100

    add comment to ActivityModule

--- app/src/main/java/lt/gecius/test_dagger2/mainactivity/ActivityModule.java
+++ app/src/main/java/lt/gecius/test_dagger2/mainactivity/ActivityModule.java
@@ -9,6 +9,7 @@ import dagger.Provides;
 import lt.gecius.test_dagger2.app.Message;
 
 @Module
+//@ActivityScope annotating whole module instead of individual method does not work
 public class ActivityModule {
 
     public static final String ACTIVITY_MSG = "ACTIVITY_MSG";
