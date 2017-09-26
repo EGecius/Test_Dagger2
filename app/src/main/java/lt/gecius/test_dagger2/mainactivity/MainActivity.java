2166fa9935872583ea1cb9946734825d898fff0a
Egis <e.gecius@gmail.com>
2017-09-26 08:03:28 +0100

    improve readability of MainActivity

--- app/src/main/java/lt/gecius/test_dagger2/mainactivity/MainActivity.java
+++ app/src/main/java/lt/gecius/test_dagger2/mainactivity/MainActivity.java
@@ -68,9 +68,13 @@ public class MainActivity extends ActionBarActivity {
         findViewById(R.id.reset_component_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                initComponent();
+                resetComponent();
             }
         });
+    }
+
+    private void resetComponent() {
+        initComponent();
     }
 
     private void initComponent() {
@@ -108,18 +112,4 @@ public class MainActivity extends ActionBarActivity {
 		return true;
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
 }
